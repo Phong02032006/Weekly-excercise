07efaee74448a7e67081bc6cf710a37c2558a85a
Nguyễn  Phong <24022823@vnu.edu.vn>
2025-02-27 08:32:14 +0700

    Bai tap simple calculator ver 0.1

---
+++ simple_calc.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Admin on 2/27/2025.
+//
+#include <iostream>
+using namespace std;
+int main() {
+  int num1, num2;
+  char operation;
+  cin >> num1 >> operation >> num2;
+  switch (operation) {
+    case '+':
+      cout << num1 + num2;
+      break;
+    case '-':
+        cout << num1 - num2;
+        break;
+    case '*':
+          cout << num1 * num2;
+          break;
+    case '/':
+            if(num2 == 0)
+              cout << "Division by zero";
+            else
+              cout << num1 / num2;
+            break;
+    case '%':
+      if(num2 == 0)
+        cout << "Division by zero";
+      else
+        cout << num1 % num2;
+      break;
+    default:
+        cout << "Invalid operation";
+        break;
+
+  }
+}
